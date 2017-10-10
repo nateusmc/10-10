bef88b01f105cddaf3554d07e639bef0f1c58a0a
Nate Garrett <nate.garrett1992@gmail.com>
2017-10-10 16:37:12 -0500

    added reference material to client.js

--- client.js
+++ client.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// $.ajax({
+//   url: '/some-endpoint',
+//   type: 'GET',
+//   beforeSend: function(req) {
+//     req.setRequestHeaders(user-pass: user=joeschmoe@business.com&pass=password);
+//   },
+//   success: function(res) {
+//     // do something with res
+//   }
+// });
